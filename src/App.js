8f2b9361db406251635e6b857ea5ccac78a2720c
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-06-15 01:12:56 -0500

    deploying react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>sup</h1>
+
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
