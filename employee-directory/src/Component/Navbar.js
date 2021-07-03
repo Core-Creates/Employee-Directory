948fddd3f24bdbc3b8f930c80cbb902ec35aae03
corry cat <corrinaalcoser@gmail.com>
2021-07-02 22:59:42 -0500

    added Navbar

---
+++ employee-directory/src/Component/Navbar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component{
+    render() {
+        return (
+
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="/logo.svg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        React Bootstrap
+                    </Navbar.Brand>
+                </Navbar>
+            </>
+
+        );
+    }
+
+}
+
+export default Navbar;
